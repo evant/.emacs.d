40acdcac026f25a717b6dc6a70b44f27179deefb
Evan Tatarka <evantatarka@gmail.com>
2012-05-28 01:06:18 -0400

    added rails support

--- init.el
+++ init.el
@@ -1,43 +1,65 @@
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
-
+;;; Helper functions
 (defun add-to-path (path)
-  (setenv "PATH" (concat path ":" (getenv "PATH")))
-  (setq exec-path (cons path exec-path)))
+  (setenv "PATH" (concat (expand-file-name path) ":" (getenv "PATH")))
+  (setq exec-path (cons (expand-file-name path) exec-path)))
 
-(defun add-to-home-path (path)
-  (add-to-path (concat (getenv "HOME") "/" path)))
-
-;; Set rbenv path\
-(add-to-home-path ".rbenv/shims")
-(add-to-home-path ".rbenv/bin")
-
-(global-set-key (kbd "M-n") 'new-frame)
-(global-set-key (kbd "C-x a r") 'align-regexp)
+(defun add-package (dir name)
+  (setq load-path (cons (expand-file-name dir) load-path))
+  (require name))
 
 (defun add-hooks (hooks f)
   (mapc
     (lambda (hook) (add-hook hook f))
     hooks))
 
+;;; Customization
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(global-set-key (kbd "M-n") 'new-frame)
+(global-set-key (kbd "C-x a r") 'align-regexp)
+
+;;; Packages
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;;; Ruby
+
+;; rails-reloaded
+(add-package "rails-reloaded" 'rails-autoload)
+
+;; set rbenv path
+(add-to-path "~/.rbenv/shims")
+(add-to-path "~/.rbenv/bin")
+
+;; other ruby files
+(mapc (lambda (file)
+       (add-to-list 'auto-mode-alist
+		    (cons (concat file "$") '(. ruby-mode))))
+     '("/.rake" "/.gemspec" "/.ru" "Rakefile" "Gemfile"))
+
+;;; Plain Text
 (defun plain-text ()
   (auto-fill-mode 1)
   (flyspell-mode 1))
 
-(defun code ()
-  (linum-mode 1)
-  (flyspell-prog-mode))
+(add-hook 'text-mode-hook 'plain-text)
 
+;;; Lisp
 (defun lisps ()
   (paredit-mode 1))
 
-(add-hook 'text-mode-hook 'plain-text)
-(add-hook 'prog-mode 'code)
+(add-hooks
+ '(emacs-lisp-mode-hook
+   clojure-mode-hook)
+ 'lisps)
+
+;;; Code
+(defun code ()
+  (linum-mode 1)
+  (flyspell-prog-mode))
 
 (add-hooks
  '(emacs-lisp-mode-hook
@@ -50,7 +72,4 @@
    clojure-mode-hook)
  'code)
 
-(add-hooks
- '(emacs-lisp-mode-hook
-   clojure-mode-hook)
- 'lisps)
+(add-hook 'prog-mode 'code)
